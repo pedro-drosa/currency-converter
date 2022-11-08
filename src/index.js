8dc27bd24ed078e8bd764a9c445cd6e890c06d61
pedro-drosa <pedro_drosa@hotmail.com>
2022-11-08 09:22:24 -0300

    refactor: main run

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
+import { prompt } from 'enquirer';
 import currencies from './shared/currencies';
+import questions from './shared/questions';
 import Calculator from './modules/Calculator';
 
-new Calculator(currencies).calculate();
+(async () => {
+  const calculator = new Calculator(currencies);
+  for (let i = 0; i <= 1; i += 1) {
+    questions[i].choices = currencies;
+  }
+  const { target, quantity } = await prompt(questions);
+  const result = calculator.csonvert(target, quantity);
+  console.log(result);
+})();
